5915f663a03894e2ec397be199eaa3601124989d
Marita <maritamovsisyan@gmail.com>
2025-08-28 14:49:12 +0400

    task 18 complete

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -155,13 +155,13 @@ suite('Unit Tests', function () {
       assert.typeOf(airlinePlane.engines, 'array');
       assert.typeOf(myCar.wheels, 'number');
     });
-  //   // #18
-  //   test('#instanceOf, #notInstanceOf', function () {
-  //     assert.fail(myCar, Plane);
-  //     assert.fail(airlinePlane, Plane);
-  //     assert.fail(airlinePlane, Object);
-  //     assert.fail(myCar.wheels, String);
-  //   });
+    // #18
+    test('#instanceOf, #notInstanceOf', function () {
+      assert.notInstanceOf(myCar, Plane);
+      assert.instanceOf(airlinePlane, Plane);
+      assert.instanceOf(airlinePlane, Object);
+      assert.notInstanceOf(myCar.wheels, String);
+    });
   });
 
   // -----------------------------------------------------------------------------
