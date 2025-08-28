0b351252e9decd51f693cd5228554aad3664b6cc
Marita <maritamovsisyan@gmail.com>
2025-08-28 16:43:34 +0400

    headless browser

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,31 +70,35 @@ suite('Functional Tests', function () {
   });
 });
 
-// const Browser = require('zombie');
+const Browser = require('zombie');
+Browser.site = 'http://0.0.0.0:3000';
 
-// suite('Functional Tests with Zombie.js', function () {
-//   this.timeout(5000);
+suite('Functional Tests with Zombie.js', function () {
+  this.timeout(5000);
+  const browser = new Browser();
 
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
+  suite('Headless browser', function () {
+    test('should have a working "site" property', function() {
+      assert.isNotNull(browser.site);
+    });
+  });
 
-//   suite('Headless browser', function () {
-//     test('should have a working "site" property', function() {
-//       assert.isNotNull(browser.site);
-//     });
-//   });
+  // suite('"Famous Italian Explorers" form', function () {
+  //   // #5
+  //   test('Submit the surname "Colombo" in the HTML form', function (done) {
+  //     assert.fail();
 
-//   suite('"Famous Italian Explorers" form', function () {
-//     // #5
-//     test('Submit the surname "Colombo" in the HTML form', function (done) {
-//       assert.fail();
+  //     done();
+  //   });
+  //   // // #6
+  //   // test('Submit the surname "Vespucci" in the HTML form', function (done) {
+  //   //   assert.fail();
 
-//       done();
-//     });
-//     // #6
-//     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-//       assert.fail();
-
-//       done();
-//     });
-//   });
-// });
+  //   //   done();
+  //   // });
+  // });
+});
