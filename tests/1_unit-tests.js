abee30528e43c2f6991185088b4de6e741e6a5e1
Marita <maritamovsisyan@gmail.com>
2025-08-28 14:18:31 +0400

    task 13 complete

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -99,16 +99,16 @@ suite('Unit Tests', function () {
 
   // // -----------------------------------------------------------------------------
 
-  // const formatPeople = function (name, age) {
-  //   return '# name: ' + name + ', age: ' + age + '\n';
-  // };
-  // suite('Strings', function () {
-  //   // #13
-  //   test('#isString, #isNotString', function () {
-  //     assert.fail(Math.sin(Math.PI / 4), 'A float is not a string');
-  //     assert.fail(process.env.PATH, 'An env variable is a string (or undefined)');
-  //     assert.fail(JSON.stringify({ type: 'object' }), 'JSON is a string');
-  //   });
+  const formatPeople = function (name, age) {
+    return '# name: ' + name + ', age: ' + age + '\n';
+  };
+  suite('Strings', function () {
+    // #13
+    test('#isString, #isNotString', function () {
+      assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string');
+      assert.isString(process.env.PATH, 'An env variable is a string (or undefined)');
+      assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string');
+    });
   //   // #14
   //   test('String #include, #notInclude', function () {
   //     assert.fail('Arrow', 'row', "'Arrow' contains 'row'");
@@ -120,7 +120,7 @@ suite('Unit Tests', function () {
   //     assert.fail(formatPeople('John Doe', 35), regex);
   //     assert.fail(formatPeople('Paul Smith III', 'twenty-four'), regex);
   //   });
-  // });
+  });
 
   // // -----------------------------------------------------------------------------
 
